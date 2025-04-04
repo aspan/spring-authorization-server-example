9f8e994bcc4e20297a1d96b5dbb88b6ef2bd18d2
Andreas Asplund <andreas@asplund.biz>
2025-04-04 10:37:32 +0200

    Upgraded vaadin to 24.7.1

--- hilla/src/main/frontend/views/login.tsx
+++ hilla/src/main/frontend/views/login.tsx
@@ -1,17 +1,18 @@
-import {ViewConfig} from '@vaadin/hilla-file-router/types.js';
-import {Button, VerticalLayout} from '@vaadin/react-components';
+import { ViewConfig } from '@vaadin/hilla-file-router/types.js';
+import { Button, VerticalLayout } from '@vaadin/react-components';
 
 export const config: ViewConfig = {
-    menu: {exclude: true},
-    flowLayout: false,
+  menu: { exclude: true },
+  flowLayout: false,
 };
 
 export default function LoginView() {
-
-    return (
-        <VerticalLayout theme="spacing padding"  style={{ alignItems: 'center' }}>
-            <h1>Login</h1>
-            <a href="/oauth2/authorization/hilla-client-oidc">Login</a>
-        </VerticalLayout>
-    );
+  return (
+    <VerticalLayout theme="spacing padding" style={{ alignItems: 'center' }}>
+      <h1>Login</h1>
+      <form id="login" action="/oauth2/authorization/hilla-client-oidc" method="get">
+        <Button onClick={async () => (document.querySelector('#login') as HTMLFormElement).submit()}>Login</Button>
+      </form>
+    </VerticalLayout>
+  );
 }
