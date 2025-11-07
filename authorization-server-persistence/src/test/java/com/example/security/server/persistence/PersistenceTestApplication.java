e3f2c3b4f9a88911c83c077c1005aede39829e90
Andreas Asplund <andreas@asplund.biz>
2025-11-07 17:34:06 +0100

    Remove references to jackson 2

--- authorization-server-persistence/src/test/java/com/example/security/server/persistence/PersistenceTestApplication.java
+++ authorization-server-persistence/src/test/java/com/example/security/server/persistence/PersistenceTestApplication.java
@@ -1,10 +1,7 @@
 package com.example.security.server.persistence;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 @EnableScheduling
